2e743d6fc9161f791cffd1f679ea1fe43b21737a
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-10-15 14:33:01 +0300

    delete unnecessary

--- Task1/Models/CustomString.cs
+++ Task1/Models/CustomString.cs
@@ -10,15 +10,6 @@ namespace Task1.Models
         public int RandomNumber { get; set; }
         public double RandomDoubleNumber { get; set; }
 
-        public void GenerateCustomString(DateTime randDate, string randLatin, string randRussian, int randNum, double randDoubleNum)
-        {
-            RandomDate = randDate;
-            RandomLatinLetters = randLatin;
-            RandomRussianLetters = randRussian;
-            RandomNumber = randNum;
-            RandomDoubleNumber = randDoubleNum;
-        }
-
         public override string ToString()
         {
             return $"{RandomDate}||{RandomLatinLetters}||{RandomRussianLetters}||{RandomNumber}||{RandomDoubleNumber}||";
