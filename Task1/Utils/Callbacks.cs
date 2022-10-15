12129847d46eda7fc310ec42671aa09c9e18f566
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-10-15 23:07:43 +0300

    added callbacks

---
+++ Task1/Utils/Callbacks.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1.Utils
+{
+    public class Callbacks
+    {
+        public delegate void joinFilesCallback(int result);
+
+        public delegate void generateFilesCallback();
+    }
+}
