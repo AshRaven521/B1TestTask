e248be7403b70404c89734d8bc1d437c1621f77d
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-10-17 22:47:47 +0300

    added XAML comments

--- Task1/Utils/Callbacks.cs
+++ Task1/Utils/Callbacks.cs
@@ -1,11 +1,26 @@
 ﻿namespace Task1.Utils
 {
+    /// <summary>
+    /// Class that contains needed callbacks
+    /// </summary>
     public class Callbacks
     {
+        /// <summary>
+        /// Delegate for function that join files in one result file
+        /// </summary>
+        /// <param name="result"></param>
         public delegate void joinFilesCallback(int result);
 
+        /// <summary>
+        /// Delegate for function that generate files
+        /// </summary>
         public delegate void generateFilesCallback();
 
+
+        /// <summary>
+        /// Delegate for function that insert data into database
+        /// </summary>
+        /// <param name="lines"></param>
         public delegate void insertIntoDB(int lines);
     }
 }
