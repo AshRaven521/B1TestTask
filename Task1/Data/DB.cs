f0786837beb89c3341648d33707a2171201d1a3f
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-10-17 22:25:59 +0300

    update DB class
    added functions that calculate sum of int numbers and median of double numbers, also added XAML comments to functions

--- Task1/Data/DB.cs
+++ Task1/Data/DB.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Task1.Utils;
-using Task1.Models;
-using System.Data.SqlClient;
 using System.Configuration;
-using System.Globalization;
-using System.Threading;
+using System.Data.SqlClient;
+using Task1.Utils;
 
 namespace Task1.Data
 {
+    /// <summary>
+    /// Class that works with Database
+    /// </summary>
     public static class DB
     {
         private static readonly string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
         private static readonly string insertIntoDBFirstPart = "INSERT INTO CustomsTable (Date, Latin, Russian, Number, DoubleNumber) ";
 
+        private static readonly string selectNumbersSum = "SELECT SUM(CAST(Number AS decimal)) FROM CustomsTable";
+
+        private static readonly string procedureName = "DoublesMedian";
+
+        /// <summary>
+        /// Function that insert given list of data into database
+        /// </summary>
+        /// <param name="stringList"> Collection of strings from file </param>
+        /// <param name="callback"> Delegate calling when 10 000 strings imported </param>
         public static void InsertIntoDB(IEnumerable<string> stringList, Callbacks.insertIntoDB callback)
         {
-            //string insertIntoDBStringSecondPart = string.Empty;
 
             using (var connect = new SqlConnection(connectionString))
             {
@@ -28,8 +34,6 @@ namespace Task1.Data
                 connect.Open();
                 foreach (var str in stringList)
                 {
-                    //if (!str.Contains("\r") && !string.IsNullOrEmpty(str))
-                    //{
                     var mas = str.Split("||");
                     if (mas is not null && mas.Length > 1)
                     {
@@ -44,17 +48,56 @@ namespace Task1.Data
                         SqlCommand insertCommand = new SqlCommand(insertIntoDBCommand, connect);
                         insertCommand.ExecuteNonQuery();
                     }
-                    //}
+
                     counter++;
                     if (counter % 10000 == 0)
                     {
                         callback(counter);
-                        //Thread.Sleep(150);
                     }
 
                 }
-                connect.Close();
             }
+        }
+        /// <summary>
+        /// Function that calculate sum of int numbers from database
+        /// </summary>
+        /// <returns> Decimal number that represents numbers sum </returns>
+        public static decimal CalculateNumbersSum()
+        {
+            decimal res = 0; 
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var selectSumCommand = new SqlCommand(selectNumbersSum, connection);
+                var prob_res = selectSumCommand.ExecuteScalar();
+                res = Convert.ToDecimal(prob_res);
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Function that calculate median of double numbers from database
+        /// </summary>
+        /// <returns> Double number that represents median </returns>
+        public static double CalculateDoubleNumbersMedian()
+        {
+            double res = 0.0;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var selectMedianCommand = new SqlCommand(procedureName, connection);
+                selectMedianCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                selectMedianCommand.CommandTimeout = 150;
+                var prob_res = selectMedianCommand.ExecuteScalar();
+                res = Convert.ToDouble(prob_res);
+            }
+
+            return res;
         }
 
     }
