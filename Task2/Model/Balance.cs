6730feebba2afcbe1741881cedfaa947d0568686
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-01-17 14:49:08 +0300

    added models

---
+++ Task2/Model/Balance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.Model
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public int CountId { get; set; }
+        public decimal InputActive { get; set; }
+        public decimal InputPassive { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public decimal OutputActive { get; set; }
+        public decimal OutputPassive { get; set; }
+    }
+}
---
+++ Task2/Model/Count.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.Model
+{
+    public class Count
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+    }
+}
