d3992d72fa6bb1f5eeaf2c28eb64402c938b6cce
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-01-21 19:17:01 +0300

    added util for working with files

---
+++ Task2/Utils/FileUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.Utils
+{
+    public static class FileUtil
+    {
+        public static async Task CopyStream(Stream stream, string downloadPath)
+        {
+            using (var fileStream = new FileStream(downloadPath, FileMode.Open, FileAccess.Write))
+            {
+                await stream.CopyToAsync(fileStream);
+            }
+        }
+
+        public static async void FillFile(string pathToTmpFile, byte[] array)
+        {
+            var fileContent = new MemoryStream(array);
+            await CopyStream(fileContent, pathToTmpFile);
+        }
+
+        public static void DeleteTempFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        public static string MapPath(string path)
+        {
+            var physicalPath = Path.GetTempFileName();
+            return physicalPath;
+        }
+
+    }
+}
